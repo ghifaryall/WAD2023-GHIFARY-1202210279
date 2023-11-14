814276c0c7dbae0c94aa09a3bf0ce89348b1302f
ghifaryall <ghifary.m.al18@gmail.com>
2023-11-14 11:23:15 +0700

    praktikum

---
+++ JURNAL MODUL 3/connect.php
@@ -0,0 +1,8 @@
+<!-- File ini berisi koneksi dengan database yang telah di import ke phpMyAdmin kalian -->
+
+<?php
+
+$koneksi = mysqli_connect("localhost:3308","root","","wad_modul3_ghifary");
+
+
+?>
