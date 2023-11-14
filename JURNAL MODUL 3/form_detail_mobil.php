0a908c6351f1134b33953bc4a362017abf7bda74
ghifaryall <116285493+ghifaryall@users.noreply.github.com>
2023-11-14 11:29:12 +0700

    form_detail_mobil.php

--- JURNAL MODUL 3/form_detail_mobil.php
+++ JURNAL MODUL 3/form_detail_mobil.php
@@ -12,9 +12,13 @@
             $id = $_GET['id'];
             // Buatlah query untuk mengambil masing-masing data berdasarkan id dari database (gunakan fungsi GET dan mysqli_fetch_assoc() 
             // serta query SELECT dan WHERE)
-            
-            
+            $sql = "SELECT * FROM showroom_mobil WHERE id=$id";
+            $query = mysqli_query($koneksi, $sql);
 
+            $ngambil = mysqli_fetch_assoc($query);
+
+            
+        
 
             //
         ?>
@@ -27,23 +31,23 @@
                             <form action="" method="POST" enctype="multipart/form-data">
                                 <!-- Tampilkan masing-masing data yang telah diambil dari database tadi -->
                                 <div class="form-floating mb-3">
-                                    <input type="string" class="form-control" name="nama_mobil" id="nama_mobil" value="Tampilkan data nama_mobil disini" disabled>
+                                    <input type="string" class="form-control" name="nama_mobil" id="nama_mobil" value="<?=$ngambil['nama_mobil']?>" disabled>
                                     <label for="nama_mobil">Nama Mobil</label>
                                 </div>
                                 <div class="form-floating mb-3">
-                                    <input type="string" class="form-control" name="brand_mobil" id="brand_mobil" value="" placeholder="Tampilkan data brand_mobil disini"disabled>
+                                    <input type="string" class="form-control" name="brand_mobil" id="brand_mobil" value="<?=$ngambil['brand_mobil']?>" placeholder="Tampilkan data brand_mobil disini"disabled>
                                     <label for="brand_mobil">Brand Mobil</label>
                                 </div>
                                 <div class="form-floating mb-3">
-                                    <input type="string" class="form-control" name="warna_mobil" id="warna_mobil" value="Tampilkan data warna_mobil disini" disabled>
+                                    <input type="string" class="form-control" name="warna_mobil" id="warna_mobil" value="<?=$ngambil['warna_mobil']?>" disabled>
                                     <label for="warna_mobil">Warna Mobil</label>
                                 </div>
                                 <div class="form-floating mb-3">
-                                    <input type="string" class="form-control" name="tipe_mobil" id="tipe_mobil" value="Tampilkan data tipe_mobil disini" disabled>
+                                    <input type="string" class="form-control" name="tipe_mobil" id="tipe_mobil" value="<?=$ngambil['tipe_mobil']?>" disabled>
                                     <label for="tipe_mobil">Tipe Mobil</label>
                                 </div>
                                 <div class="form-floating mb-3">
-                                    <input type="number" class="form-control" name="harga_mobil" id="harga_mobil" value="Tampilkan data harga_mobil disini"disabled>
+                                    <input type="number" class="form-control" name="harga_mobil" id="harga_mobil" value="<?=$ngambil['harga_mobil']?>"disabled>
                                     <label for="harga_mobil">Harga Mobil </label>
                                 </div>
                                 <a name="update" id="update" href="form_update_mobil.php?id=<?php echo $id ?>" class="btn btn-warning mb-3 mt-3 w-100">Edit</a>
